8ea3d2e718ad1fee7b361fc9f20ed676bad57415
odedmo <odedmoran222@gmail.com>
2024-02-18 22:03:24 +0200

    dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import { LayoutProps } from '@/.next/types/app/layout';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+const Layout = (props: LayoutProps) => {
+  const { children } = props;
+
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
